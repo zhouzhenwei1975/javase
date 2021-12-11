15d80e82e58e93b14e05bd629bf2ebc7ecf30cde
zhouzhenwei1975 <591264621@qq.com>
2021-12-11 18:15:44 +0800

    我的第一个Java程序

---
+++ java-foundational/src/main/java/net/zzw/javase/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.zzw.javase.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author zzw dgzzw1@163.com
+ * @version 2021/12/11 下午6:50
+ * @since JDK11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with macOS & JDK11 & Maven & git & IDEA");
+	}
+}
